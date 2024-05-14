6aaf3a27b7e2081eed7db1a82068d72448e787ec
luizbelmudes <luizbeds@gmail.com>
2024-05-13 22:06:28 -0300

    Subindo Contador

--- desafio-fluxo/src/contador/Contador.java
+++ desafio-fluxo/src/contador/Contador.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+        int primeiroParametro;
+        int segundoParametro;
         System.out.println("Digite o primeiro Parametro");
-        int primeiroParametro = terminal.nextInt();
-        System.out.println("Digite o segundo Parametro");
-        int segundoParametro = terminal.nextInt();
+        /* Try-Catch abaixo pega a Exception se
+        * o tipo de parametro nao for inteiro */
+        try {
+            primeiroParametro = terminal.nextInt();
+            System.out.println("Digite o segundo Parametro");
+            segundoParametro = terminal.nextInt();
+        } catch (java.util.InputMismatchException ex){
+            System.out.println("Parâmetro inválido! Digite um número inteiro");
+            return;
+        }
+        /* Try-Catch abaixo pega a Exception se
+         * o a combinação de parametros não for válida */
         try {
              contar(primeiroParametro, segundoParametro);
          } catch (ParametrosInvalidosException ex) {
@@ -19,7 +30,7 @@ public class Contador {
 
     static void contar(int primeiroParametro, int segundoParametro) throws ParametrosInvalidosException {
         int contagem = segundoParametro - primeiroParametro;
-        if (contagem < 0) {
+        if (contagem <= 0) {
             throw new ParametrosInvalidosException();
         }
         for(int index = 1;index <= contagem;index++){
